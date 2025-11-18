b5be086d15fc850d0aaf681a46b3713195318e50
Adnan Abdul <adnanabdul@Adnans-MacBook-Pro.local>
2025-11-18 16:09:39 +0300

    changes

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 
+function calculateTax(amount){
+    return amount * 0.1
+}
 
+function convertToUpperCase(text){
+    return text.toUppercase()
+}
+function findMaximum(num1, num2){
+    if (num1 > num2){
+        return num1
+    }
 
+    else {
+        return num2}
+}
+function isPalindrome(word){
+     let reversed= word.split('').reverse().join('')
+     return word == reversed
+
+    }
+
+function calculateDiscountedPrice(originalprice ,dicountpercentage){
+    return originalprice- (originalprice-dicountpercentage)
+}
 
 
 // This is required for the test to function properly  
